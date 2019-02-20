37220b10e9ef5720e4212b7f2a4407865f77eeee
fuleige <39784621+fuleige@users.noreply.github.com>
2019-02-20 09:05:56 +0800

    Add files via upload

---
+++ BountyHunter/miniprogram/pages/bountydetail/bountydetail.js
@@ -0,0 +1,71 @@
+// pages/bountydetail/bountydetail.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    people_earn:3,
+    people_empty:93,
+    leave_time:24,
+    task_desc:"必须是新用户",
+    shopname:"小猪佩奇",
+    shopid:2763364
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+    wx.stopPullDownRefresh();
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
